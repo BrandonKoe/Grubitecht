2dc406a0eca839f547b96ca1cb24abb03612308f
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-03-06 14:48:30 -0600

    Initial custom brush
    Created a test brush to experiment with the Custom Brush System as a way to create 3D tilemaps.

---
+++ Grubitecht/Assets/Scripts/EditorTools/TestGridBrush.cs
@@ -0,0 +1,38 @@
+/*****************************************************************************
+// File Name : TestGridBrush.cs
+// Author : Brandon Koederitz
+// Creation Date : March 6, 2025
+//
+// Brief Description : A test script to try out making a custom grid brush.
+// Basing this test off of a unity forum post: https://discussions.unity.com/t/getting-started-creating-a-custom-tile-palette-brush/816589
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor.Tilemaps;
+using UnityEditor;
+#endif
+
+[CreateAssetMenu]
+[CustomGridBrush(false, true, false, "Test Grid Brush")]
+public class TestGridBrush : GridBrush
+{
+    #if UNITY_EDITOR
+    public static void CreateTestBrush()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Test Brush", "New Test Brush", "asset", "Saved Test Brush");
+        if (path == null) { return; }
+
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TestGridBrush>(), path);
+    }
+    #endif
+
+    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        base.Paint(gridLayout, brushTarget, position);
+        Debug.Log(position);
+        Debug.Log(brushTarget);
+    }
+}
