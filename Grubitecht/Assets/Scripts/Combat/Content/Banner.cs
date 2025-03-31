27b8a4e96e4590cf85d4a2105a6254833bee9ddc
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-03-31 17:38:07 -0500

    Player Info Final

--- Grubitecht/Assets/Scripts/Combat/Content/Banner.cs
+++ Grubitecht/Assets/Scripts/Combat/Content/Banner.cs
@@ -5,6 +5,8 @@
 //
 // Brief Description : Banner of the grubs that empowers all nearby grub structures.
 *****************************************************************************/
+using Grubitecht.UI.InfoPanel;
+using Grubitecht.World.Objects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +16,40 @@ namespace Grubitecht.Combat
     public class Banner : Effector
     {
         [SerializeField] private int attackBoost;
+
+        #region Component References
+        [SerializeReference, HideInInspector] private SelectableObject selectableObject;
+
+        /// <summary>
+        /// Assign component references on reset.
+        /// </summary>
+        protected override void Reset()
+        {
+            base.Reset();
+            selectableObject = GetComponent<SelectableObject>();
+        }
+        #endregion
+
+        /// <summary>
+        /// Adds this component's info getter so that the attack boost can be displayed.
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
+            if (selectableObject != null)
+            {
+                selectableObject.AddInfoGetter(InfoGetter);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (selectableObject != null)
+            {
+                selectableObject.RemoveInfoGetter(InfoGetter);
+            }
+        }
 
         /// <summary>
         /// Applies an attack boost to attackers within the banner's range.
@@ -31,6 +67,18 @@ namespace Grubitecht.Combat
         protected override void RemoveBuff(Attacker buffedAttacker)
         {
             buffedAttacker.AttackStat -= attackBoost;
+        }
+
+        /// <summary>
+        /// Provides this component's values to display on the info panel when selected.
+        /// </summary>
+        /// <returns>The info about this component to display when this object is selected.</returns>
+        private InfoValueBase[] InfoGetter()
+        {
+            return new InfoValueBase[]
+            {
+                new NumValue(attackBoost, 2, "Attack Boost"),
+            };
         }
     }
 }
