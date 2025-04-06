78d63a8cdd0244bea62cde2719a9ae4414414c27
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-04-06 15:41:16 -0500

    Screen Canvas Conversion
    Switch the indicators to using the screen space canvas so that they can always be clamped to the screen.

--- Grubitecht/Assets/Scripts/Statics/MathHelpers.cs
+++ Grubitecht/Assets/Scripts/Statics/MathHelpers.cs
@@ -29,6 +29,22 @@ namespace Grubitecht
         }
 
         /// <summary>
+        /// Clamps all values of a vector3 between two given ints.
+        /// </summary>
+        /// <param name="input">The vector to clamp.</param>
+        /// <param name="min">The min possible value.</param>
+        /// <param name="max">The max possible value.</param>
+        /// <returns>The Vector3Int with all of it's components clamped between the min and max values.</returns>
+        public static Vector3 V3Clamp(Vector3 input, int min, int max)
+        {
+            input.x = Mathf.Clamp(input.x, min, max);
+            input.y = Mathf.Clamp(input.y, min, max);
+            input.z = Mathf.Clamp(input.z, min, max);
+            return input;
+        }
+
+
+        /// <summary>
         /// Gets the absolute value of a vector's components.
         /// </summary>
         /// <param name="input">The input vector.</param>
@@ -78,13 +94,23 @@ namespace Grubitecht
         }
 
         /// <summary>
-        /// Converts a vector to an angle in degrees.
+        /// Converts a vector in unity world space to an angle in degrees.
         /// </summary>
         /// <param name="vector">The vector to convert to an angle.</param>
         /// <returns>The angle that corresponds to that vector.</returns>
         public static float VectorToDegAngleWorld(Vector2 vector)
         {
             return Mathf.Atan2(vector.x, vector.y) * Mathf.Rad2Deg;
+        }
+
+        /// <summary>
+        /// Converts a vector to an angle in degrees.
+        /// </summary>
+        /// <param name="vector">The vector to convert to an angle.</param>
+        /// <returns>The angle that corresponds to that vector.</returns>
+        public static float VectorToDegAngle(Vector2 vector)
+        {
+            return Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
         }
 
         /// <summary>
--- Grubitecht/Assets/Scripts/UI/WavePredictor.cs
+++ Grubitecht/Assets/Scripts/UI/WavePredictor.cs
@@ -17,10 +17,22 @@ namespace Grubitecht.UI
         [SerializeField] private Vector3 baseOffset = new Vector3(0, 1, 0);
         [SerializeField] private Image enemyPredictionImage;
         [SerializeField] private Image timerImage;
+        [SerializeField] private RectTransform backImageTransform;
         [SerializeField] private TMP_Text enemyNumberText;
-        [SerializeField] private float offset;
-        [SerializeField] private float angularOffset;
+        [SerializeField, Tooltip("The distance from the central position this object should offset itself.")] 
+        private float offset;
+        [SerializeField, Tooltip("The angle in degrees that each predictor will be offset from each other rotating " +
+            "around the origin point")] 
+        private float angularOffset;
+        [SerializeField] private float padding;
 
+        private Vector3 basePosition;
+        private int typeNumber;
+
+
+        #region Properties
+        private RectTransform rectTransform => (RectTransform)transform;
+        #endregion
         /// <summary>
         /// Initializes this object with values when it spawns.
         /// </summary>
@@ -28,18 +40,20 @@ namespace Grubitecht.UI
         /// <param name="basePosition">The position that this UI element reperesents in world space.</param>
         /// <param name="enemyNumber">The number of enemies of this type that will be in the next wave.</param>
         /// <param name="duration">The amount of time until this wave spawns.</param>
-        /// <param name="typeNumber">The number of other predictors.  Used to offset this predictor by an angle.</param>
-        public void Initialize(Sprite predictionSprite, Vector3 basePosition, int enemyNumber, float duration, int typeNumber)
+        /// <param name="typeNumber">
+        /// The number of other predictors.  Used to offset this predictor by an angle.
+        /// </param>
+        public void Initialize(Sprite predictionSprite, Vector3 basePosition, int enemyNumber, float duration, 
+            int typeNumber)
         {
             enemyPredictionImage.sprite = predictionSprite;
             enemyNumberText.text = enemyNumber.ToString();
-            // Update the object's angular offset here.
-            transform.position = basePosition + baseOffset;
-            float angle = angularOffset * typeNumber;
-            Vector2 offsetAngle = MathHelpers.DegAngleToUnitVector(90 - angle);
-            //Debug.Log(offsetAngle);
-            transform.localPosition += (Vector3)offsetAngle * offset;
-
+            this.typeNumber = typeNumber;
+            this.basePosition = basePosition;
+            // Subscribe to the camera pan event because this object's position should only ever be updated if
+            // the camera moves and it needs to be.
+            CameraPanner.OnCameraPan += UpdatePosition;
+            UpdatePosition();
             StartCoroutine(LifeCycle(duration));
         }
 
@@ -59,6 +73,54 @@ namespace Grubitecht.UI
                 timer -= Time.deltaTime;
                 yield return null;
             }
+            Unload();
+        }
+
+        /// <summary>
+        /// Updates the position of this object to be within the bounds of the screen.
+        /// </summary>
+        private void UpdatePosition()
+        {
+            // The position in screen space that represents the position of the spawn point this
+            // predictor represents.
+            Vector2 realOriginPos = RectTransformUtility.WorldToScreenPoint(Camera.main,
+                basePosition + baseOffset);
+            Vector2 margins = rectTransform.sizeDelta + (2 * offset * Vector2.one);
+            // The actual base position that this predictor will be centered around after it has been clamped to
+            // be within the bounds of the canvas.
+            Vector2 displayOriginPos = new Vector2(
+                Mathf.Clamp(realOriginPos.x, margins.x, Screen.width - margins.x),
+                Mathf.Clamp(realOriginPos.y, offset + margins.y, Screen.height - margins.y));
+            //Debug.Log(rectTransform.sizeDelta);
+            float angle = angularOffset * typeNumber;
+            Vector2 offsetAngle = MathHelpers.DegAngleToUnitVector(90 - angle);
+            //Debug.Log(offsetAngle);
+            rectTransform.anchoredPosition = displayOriginPos + offsetAngle * offset;
+            //transform.localPosition += (Vector3)offsetAngle * offset;
+
+            UpdatePointerRotation(realOriginPos);
+        }
+
+        /// <summary>
+        /// Updates the rotation of this predictor's background image so that it points towards the real position 
+        /// of the spawn point.
+        /// </summary>
+        /// <param name="realOriginPos">The screen position that the pointer should point toward.</param>
+        private void UpdatePointerRotation(Vector2 realOriginPos)
+        {
+            Vector2 pointingVector = realOriginPos - rectTransform.anchoredPosition;
+            float angle = MathHelpers.VectorToDegAngle(pointingVector);
+            Vector3 eulers = backImageTransform.eulerAngles;
+            eulers.z = angle;
+            backImageTransform.eulerAngles = eulers;
+        }
+
+        /// <summary>
+        /// Handles behaviour that should happen when the object reaches the end of it's life cycle.
+        /// </summary>
+        private void Unload()
+        {
+            CameraPanner.OnCameraPan -= UpdatePosition;
             Destroy(gameObject);
         }
     }
