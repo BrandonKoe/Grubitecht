90d34240896dffa45cc9f70e6a39ae45a5a52620
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-05-03 20:21:41 -0500

    Settings Menu Start

---
+++ Grubitecht/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,29 @@
+/*****************************************************************************
+// File Name : GameSettings.cs
+// Author : Brandon Koederitz
+// Creation Date : May 3, 2025
+//
+// Brief Description : Controls the settings that affect gameplay.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grubitecht
+{
+    public class GameSettings : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Grubitecht/Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -0,0 +1,18 @@
+/*****************************************************************************
+// File Name : SettingsMenu.cs
+// Author : Brandon Koederitz
+// Creation Date : May 3, 2025
+//
+// Brief Description : Controls the settings menu and allows it to modify with game settings.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grubitecht.UI
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        
+    }
+}
